2783a969a0a7f0adbf4f48db4ef1a7cbf391c226
James Hannah <james.hannah@bytemark.co.uk>
2014-10-03 14:49:07 +0100

    New tests

---
+++ mauve/mauve_test.go
@@ -0,0 +1,70 @@
+package mauve
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreateAlert(t *testing.T) {
+	//func CreateAlert(id string, raise string, clear string, subject string, summary string, detail string, suppress string) *Alert {
+	testId := "exampleTesting"
+	testRaiseTime := "now"
+	testClearTime := ""
+	subject := "subject.example.com"
+	summary := "This is a test of the CreateAlert function"
+	detail := "This is a test of the CreateAlert function and has some text in the detail"
+	suppress := ""
+	a := CreateAlert(testId,testRaiseTime,testClearTime,subject,summary,detail,suppress)	
+	if *a.Id != testId {
+		t.Errorf("Created alert ID didn't match")
+	}
+	//todo: incomplete test
+}
+
+func TestCreateUpdate(t *testing.T) {
+	sourceTest := "test.example.com"
+	replace := false
+	fakeAlert := CreateAlert("id","now","","","","","")
+	u := CreateUpdate(sourceTest, replace, fakeAlert)
+	if *u.Source != sourceTest {
+		t.Errorf("Mismatched source after createUpdate")
+	}
+	// todo: incomplete test
+}
+
+func TestParseTimeWithNow(t *testing.T) {
+	testCases := map[string]time.Duration {
+		"now": 0,
+		"10s": time.Duration(10)*time.Second,
+		"3m": time.Duration(3)*time.Minute,
+		"6h": time.Duration(6)*time.Hour,
+	}
+	for tVal,expected := range testCases {
+		xres := ParseTimeWithNow(tVal)
+		if xres != time.Duration(expected) {
+			t.Errorf("%d does not match %v for test [%s]", xres, expected, tVal)
+		}
+	}
+}
+
+type ttAlertSSID struct {
+	Source string
+	Subject string
+	Id string
+}
+
+func TestAlertTopic(t *testing.T) {
+	testCases := map[ttAlertSSID]string {
+		ttAlertSSID{"tSource","tSubject","tId"}: "tSource/tSubject/tId", // normal
+		ttAlertSSID{"asdax/foo", "bar", "baz"}: "asdax_foo/bar/baz", // contains a slash in the source that should be escaped
+		ttAlertSSID{"asda", "barx/foo", "baz"}: "asda/barx_foo/baz", // contains a slash in the subject that should be escaped
+		ttAlertSSID{"asda", "bar", "bazx/foo"}: "asda/bar/bazx_foo", // contains a slash in the id that should be escaped
+	}
+	for tass, expt := range testCases {
+		tAlert := CreateAlert(tass.Id, "", "", tass.Subject, "", "", "")
+		xret := AlertTopic(tAlert, tass.Source)
+		if xret != expt {
+			t.Errorf("Failed to get proper alert topic t, %s is not %s", xret, expt)
+		}
+	}
+}
